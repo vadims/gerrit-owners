ef96583c19c8d7715bb7cf455bd6ec98a1014202
Ugo Di Girolamo <ugo@compass.com>
2015-03-16 14:51:12 -0400

    Upgrade to gerrit v2.10
    Looks like we need to move away from @Listen and have our own guice module.

--- gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
+++ gerrit-owners-autoassign/src/main/java/com/vmware/gerrit/owners/common/ReviewerManager.java
@@ -4,11 +4,13 @@
 package com.vmware.gerrit.owners.common;
 
 import com.google.gerrit.common.errors.EmailException;
+import com.google.gerrit.extensions.api.changes.AddReviewerInput;
 import com.google.gerrit.extensions.restapi.RestApiException;
 import com.google.gerrit.reviewdb.client.Account;
 import com.google.gerrit.reviewdb.client.Change;
 import com.google.gerrit.server.CurrentUser;
 import com.google.gerrit.server.change.ChangeResource;
+import com.google.gerrit.server.change.ChangesCollection;
 import com.google.gerrit.server.change.PostReviewers;
 import com.google.gerrit.server.project.ChangeControl;
 import com.google.gerrit.server.project.NoSuchChangeException;
@@ -29,25 +31,29 @@ public class ReviewerManager {
 
   private final ChangeControl.GenericFactory changeControlFactory;
 
+  private final ChangesCollection changesCollection;
+
   @Inject
   public ReviewerManager(Provider<CurrentUser> currentUserProvider,
                          Provider<PostReviewers> postReviewersProvider,
-                         ChangeControl.GenericFactory changeControlFactory) {
+                         ChangeControl.GenericFactory changeControlFactory,
+                         ChangesCollection changesCollection) {
     this.currentUserProvider = currentUserProvider;
     this.postReviewersProvider = postReviewersProvider;
     this.changeControlFactory = changeControlFactory;
+    this.changesCollection = changesCollection;
   }
 
   public void addReviewers(Change change, Collection<Account.Id> reviewers) throws ReviewerManagerException {
     try {
       PostReviewers postReviewers = postReviewersProvider.get();
       ChangeControl changeControl = changeControlFactory.controlFor(change, currentUserProvider.get());
-      ChangeResource changeResource = new ChangeResource(changeControl);
+      ChangeResource changeResource = changesCollection.parse(changeControl);
 
       // HACK(vspivak): Using PostReviewers is probably inefficient here, however it has all the hook/notification
       // logic, so it's easier to call it then to mimic/copy the logic here.
       for (Account.Id accountId : reviewers) {
-        PostReviewers.Input input = new PostReviewers.Input();
+        AddReviewerInput input = new AddReviewerInput();
         input.reviewer = accountId.toString();
         postReviewers.apply(changeResource, input);
       }
